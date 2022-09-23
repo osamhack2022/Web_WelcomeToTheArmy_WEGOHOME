146ac5a5f735ae5c3e6e752f006cc9fe93672589
giopaik <giopaik@naver.com>
2022-09-23 15:28:51 +0200

    INIT(frontend): Create Basic Components
    - 사용할 것으로 예상되는 컴포넌트들 생성
    - 부트스트랩 설치

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import './index.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.esm.min.js'
 
 createApp(App).mount('#app')
