16498f5c92ffd30004fadb95cc4707efb819c016
giopaik <giopaik@naver.com>
2022-09-23 16:43:43 +0200

    FEAT(route): Add Router
    라우팅 기능 추가
    로그인 페이지 샘플과 404 에러 페이지 생성
    나눔고딕 폰트 추가

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="background">
+    <section class="login-card">
+        <h1>LOGO</h1>
+        <form action="">
+            <div class="input-area">
+                <input type="text" name="id" id="id" 
+                autocomplete="off" required/>
+                <label for="id">군번</label>
+            </div>
+            <div class="input-area">
+                <input type="password" name="pw" id="pw"
+                autocomplete="off" required/>
+                <label for="pw">비밀번호</label>
+            </div>
+            <div class="button-area">
+                <button type="submit">LOGIN</button>
+            </div>
+        </form>
+        <div class="caption">
+            <router-link to="/recovery" />
+        </div>
+    </section>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.background {
+    /* font-family:  */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background: url("via.placeholder.com/1280") no-repeat center;
+    background-size: cover;
+}
+.background::before {
+    content: "";
+    position: absolute;
+    z-index: 1;
+    top: 0; right: 0; bottom: 0; left: 0;
+    background-color: rgba(0,0,0,.8)
+}
+
+.login-card {position: relative; z-index: 2;}
+
+h1 {
+    font-size: 32px; color: #FFFFFF;
+    text-align: center;
+}
+.input-area {
+    width: 400px; position: relative;
+    margin-top: 20px;
+}
+.input-area:first-child {
+    margin-top: 0;
+}
+.input-area input {
+    width: 100%;
+    padding: 20px 10px 10px;
+    background-color: transparent;
+    border: none;
+    border-bottom: 1px solid #999;
+    font-size: 18px; color: #fff;
+    outline: none;
+}
+.input-area label {
+    position: absolute; left: 10px; top: 15px;
+    font-size: 18px; color: #999;
+    transition: top .5s ease;
+}
+.input-area input:focus + label,
+.input-area input:valid + label {
+    top: -2px;
+    font-size: 13px; color: #166cea;
+}
+
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,15 @@
-import { createApp } from 'vue'
+import { createApp, h } from 'vue'
 import App from './App.vue'
 import './index.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.esm.min.js'
 
-createApp(App).mount('#app')
+import router from "./router/router"
+
+const app  = createApp({
+    render: () => h(App)
+});
+
+app.use(router);
+app.mount('#app');
