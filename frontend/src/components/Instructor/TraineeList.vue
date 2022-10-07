9e2e0fa488b42f6314692bb570afcb68969a10e0
giopaik <giopaik@naver.com>
2022-10-07 13:42:14 +0200

    FEAT(frontend): create instructor trainee management page

--- frontend/src/components/Instructor/TraineeList.vue
+++ frontend/src/components/Instructor/TraineeList.vue
@@ -1,19 +1,71 @@
 <template>
-    <h1>Trainee List</h1>
+    <section class="search-menu">
+        <h3>훈련병 검색하기</h3>
+    </section>
+    <section class="button-area">
+        <button class="btn btn-outline-dark">엑셀 업로드</button>
+        <button class="btn btn-outline-dark">훈련병 추가</button> 
+    </section>
+    <section class="notice-area">
+        <p>훈련병을 클릭하면 자세한 정보를 볼 수 있습니다.</p>
+    </section>
+    <section class="list-area">
+        <TraineeListCard />
+        <TraineeListCard />
+        <TraineeListCard />
+        <TraineeListCard />
+    </section>
 </template>
 
 <script>
 import Nav from "@components/Instructor/Nav.vue";
+import TraineeListCard from "./TraineeListCard.vue";
 
 export default {
     components: {
-        Nav,
-    }
+    Nav,
+    TraineeListCard,
+}
 }
 </script>
 
 <style scoped>
-    .background {
-        
-    }
+* {
+    margin: 0;
+    padding: 0;
+}
+.search-menu {
+    width: 100%; height: 340px;
+    padding: 5px;
+    background-color: #ABABAB;
+}
+.button-area {
+    width: 100%;
+    margin-top: 10px;
+    display: inline-block;
+}
+
+.btn {
+    float: right;
+    padding: 5px;
+    margin-left: 5px;
+
+    font-size: 12px;
+    font-weight: bold;
+}
+
+.notice-area {
+    width: 100%;
+    display: inline-block;
+}
+
+.notice-area > p {
+    font-size: 12px;
+    float: right;
+}
+
+.list-area > * {
+    margin: 15px 0px;
+}
+
 </style>
