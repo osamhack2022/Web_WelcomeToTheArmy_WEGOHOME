d6a7b60238076fbdb7a6aa5ebaec881907e088d8
giopaik <giopaik@naver.com>
2022-10-10 08:46:24 +0200

    FEAT(frontend): Create Instructor Manage Page

---
+++ frontend/src/components/Instructor/InstructorListCard.vue
@@ -0,0 +1,146 @@
+<template>
+    <div class="trainee-card">
+        <section class="head" @click="toggleBody($event)">
+            <div class="head-left">
+                <img class="trainee-image rounded-circle" src="@/assets/images/instructor_profile_example.jpg" alt="훈련병 프로필 이미지" />
+                <div class="trainee-profile">
+                    <h1 class="trainee-name">상사 김훈련</h1>
+                    <h1 class="trainee-position">신병 4대대 3중대 1소대장</h1>
+                </div>
+            </div>
+            <div class="head-right">
+                <p class="flag"></p>
+            </div>
+        </section>
+        <form class="form-c">
+            <section class="body" style="display:none;">
+                <div class="data-area">
+                    <div class="data-field">
+                        <p class="data-label">이름</p>
+                        <input class="data-input data-text" type="text" required>
+                    </div>
+                    <div class="data-field">
+                        <p class="data-label">계급</p>
+                        <input class="data-input data-text" type="text" required>
+                    </div>
+                    <div class="data-field">
+                        <p class="data-label">군번</p>
+                        <input class="data-input data-text" type="text" required>
+                    </div>
+                    <div class="data-field">
+                        <p class="data-label">직책</p>
+                        <input class="data-input data-text" type="text" required>
+                    </div>
+                    <div class="data-field">
+                        <p class="data-label">소속</p>
+                        <input class="data-input data-text" type="text" required>
+                    </div>
+                    <div class="data-field">
+                        <p class="data-label">연락처</p>
+                        <input class="data-input data-text" type="tel" pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}" placeholder="010-1234-5678" required>
+                    </div>
+                </div>
+            </section>
+            <section class="button-area">
+                <button class="btn btn-primary">수정</button> 
+                <button class="btn btn-danger" type="button">삭제</button>
+                <button class="btn btn-outline-dark" @click="cancelChange($event)" type="button">취소</button>
+            </section>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            toggleBody(event) {
+                const card = event.currentTarget.parentNode;
+                const body = card.childNodes[1].firstChild;
+                const buttons = card.childNodes[1].childNodes[1];
+                if (body.style.display === "none") {
+                    card.style.height = "auto";
+                    body.style.display = "block";
+                    buttons.style.display = "inline-block";
+                }
+                else {
+                    card.style.height = "60px";
+                    body.style.display = "none";
+                    buttons.style.display = "none";
+                }
+            },
+            cancelChange(event) {
+                const card = event.currentTarget.parentNode.parentNode.parentNode;
+                const body = card.childNodes[1].firstChild;
+                const buttons = card.childNodes[1].childNodes[1];
+                card.style.height = "60px";
+                body.style.display = "none";
+                buttons.style.display = "none";
+            }
+        }
+    }
+</script>
+
+<style scoped>
+* {
+    margin: 0; padding: 0;
+}
+
+.trainee-card {
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 8px;
+    height: 60px;
+    overflow: hidden;
+}
+
+.head {
+    background-color: #D9D9D9;
+    width: 100%; height: 60px;
+    display: inline-block;
+    align-items: center;
+}
+
+.head > div {
+    height: 100%;
+    display: flex;
+    align-items: center;
+}
+
+.head-left {
+    float: left;
+}
+
+.trainee-image {width: 45px; height: 45px; margin-left: 10px; object-fit: cover;}
+.trainee-profile {margin-left: 3px;}
+.trainee-name {font-size: 20px; font-weight: bold; }
+.trainee-position {font-size: 15px;}
+
+.head-right {
+    float: right;
+}
+
+.flag {
+    margin-right: 10px; 
+    text-align: right; font-size: 25px;
+}
+
+.body {
+    width: 100%;
+    padding: 10px 0px;
+}
+
+.data-area { width: 100%; align-items: center; text-align: center;}
+.data-field { display: inline-block; width: 40%; margin: 3px 5%;}
+.data-label { float: left; font-size: 15px; font-weight:bold; }
+.data-input { float: right; width: 150px;}
+.data-text {background: #F9F9F9; border: solid 1px;}.data-text:invalid {border-color: red;}
+
+.button-area { padding-right: 10px; display: none; width: 100%; margin-bottom: 10px; }
+.btn {
+    float: right;
+    padding: 5px 20px;
+    margin-left: 5px;
+
+    font-size: 15px;
+}
+
+</style>
---
+++ frontend/src/components/Instructor/InstructorListCreateModal.vue
@@ -0,0 +1,131 @@
+<template>
+    <div class="modal-background" v-if="visible" @click.self="handlebackgroundClick">
+        <div class="modal-window">
+            <h1 class="title">훈육관 추가하기</h1>
+            <form target="dummy">
+                <iframe id="dummy" name="dummy" style="display: none;" />
+                <div class="data-area">
+                    <div class="data-field">
+                        <p class="data-label">이름</p>
+                        <input class="data-input data-text" type="text" v-model="instructor.name" required>
+                    </div>
+                    <div class="data-field">
+                        <p class="data-label">계급</p>
+                        <input class="data-input data-text" type="text" v-model="instructor.rank" required>
+                    </div>
+                    <div class="data-field">
+                        <p class="data-label">군번</p>
+                        <input class="data-input data-text" type="text" v-model="instructor.manager_id" required>
+                    </div>
+                    <div class="data-field">
+                        <p class="data-label">직책</p>
+                        <input class="data-input data-text" type="text" v-model="instructor.position" required>
+                    </div>
+                    <div class="data-field">
+                        <p class="data-label">소속</p>
+                        <input class="data-input data-text" type="text" v-model="instructor.battalion" required>
+                    </div>
+                    <div class="data-field">
+                        <p class="data-label">연락처</p>
+                        <input class="data-input data-text" type="tel" v-model="instructor.tel" pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}" placeholder="010-1234-5678" required>
+                    </div>
+                </div>
+                <div class="button-area">
+                    <button class="btn btn-primary" @click="createInstructor" type="submit">생성</button>
+                    <button class="btn btn-danger" @click="$emit('update', !visible)">취소</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+    
+<script>
+import useAxios from "@app_modules/axios.js"
+
+const { axiosPost } = useAxios()
+
+export default {
+    name: 'InstructorCreateModal',
+    data() {
+        return {
+            instructor:  {
+                manager_id: null,
+                name: null,
+                rank: null,
+                position: null,
+                battalion: null,
+                company: null,
+                platoon: null,
+                tel: null,
+            }
+        }
+    },
+    props: {
+        visible: {
+            type: Boolean,
+            require: true,
+            default: false
+        },
+    },
+    methods: {
+        handlebackgroundClick(){
+            this.$emit('update', false)
+        },
+        createInstructor(){
+            alert("hey")
+            const onSuccess = (data) => {
+                alert(this.instructor.name + "훈육관이 성공적으로 추가되었습니다!")
+            }
+            const onFailed = (data) => {
+                alert("훈육관을 추가하는데 실패하였습니다.")
+            }
+            axiosPost("/createInstructor", this.instructor, onSuccess, onFailed)
+        },
+    }
+}
+</script>
+
+<style scoped>
+.modal-background {
+    background-color: rgba(0,0,0,0.7);
+    top: 0; right: 0; bottom: 0; left: 0;
+    position: fixed;
+    overflow: auto;
+    margin: 0;
+    z-index: 1020;
+}
+
+.modal-window {
+    left: 50%; top: 50%;
+    width: 700px;
+    border-radius: 10px;
+    transform: translate(-50%, -50%);
+    position: absolute;
+    background-color: white;
+    margin-bottom: 50px;
+    padding: 20px 10px;
+}
+
+.title {
+    font-family: ROKAF Sans;
+    font-size: 25px;
+    text-align: center;
+    margin-bottom: 25px;
+}
+
+.data-area { width: 100%; align-items: center; text-align: center;}
+.data-field { display: inline-block; width: 40%; margin: 3px 5%;}
+.data-label { float: left; font-size: 15px; font-weight:bold; }
+.data-input { float: right; width: 150px;}
+.data-text {background: #F9F9F9; border: solid 1px;}.data-text:invalid {border-color: red;}
+
+.btn {
+    margin: 0px 3px;
+}
+.button-area {
+    width: 100%;
+    text-align: center;
+    align-items: center;
+}
+</style>
+
