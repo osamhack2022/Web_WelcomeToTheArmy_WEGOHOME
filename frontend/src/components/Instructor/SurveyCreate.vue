8f17baa6dbef4a8597f68e6f26a7c700ac321e27
giopaik <giopaik@naver.com>
2022-10-19 13:38:55 +0200

    FEAT(frontend): Add Instructor Survey Create Feature

--- frontend/src/components/Instructor/SurveyCreate.vue
+++ frontend/src/components/Instructor/SurveyCreate.vue
@@ -2,13 +2,15 @@
 <section class="title-area">
     <h1 class="title">새 조사전달 만들기</h1>
     <span class="title">제목</span>
-    <input type="text" class="data-title"/>
+    <input type="text" class="data-title" v-model="survey.title" />
 </section>
 <section class="questions-area">
-    <QuestionCard />
+    <div v-for="(question, i) in questionList">
+        <QuestionCard :questionIndex="i"/>
+    </div>
 </section>
 <section class="button-area">
-    <button class="btn btn-secondary">질문 추가</button>
+    <button class="btn btn-secondary" @click="addQuestion">질문 추가</button>
     <button class="btn btn-primary">작성 완료</button>
 </section>
 </template>
@@ -16,9 +18,25 @@
 <script>
 import QuestionCard from "@components/Instructor/SurveyCreateQuestionCard.vue"
 export default {
+    data() {
+        return {
+            questionList: [""],
+            survey: {
+                title: "",
+            }
+        }
+    },
     components: {
         QuestionCard,
-    }
+    },
+    methods: {
+        addQuestion() {
+            this.questionList.push("")
+        },
+        removeQuestion(index) {
+            this.questionList.splice(index, 1)
+        }
+    },
 }
 </script>
 
@@ -30,6 +48,7 @@ export default {
     margin-left: 5px;
     font-size: 25px;
     width: 60%;
+    border-radius: 3px;
 }
 
 .button-area {
